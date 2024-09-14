f1e1367edacc44007788d0e18b341721dae1d706
Elvis <elvis.omondi@student.moringaschool.com>
2024-09-14 17:05:38 +0300

    commit

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,25 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+
+function EyesOnMe() {
+  // Event handler for the focus event
+  const handleFocus = () => {
+    console.log('Good!');
+  };
+
+  // Event handler for the blur event
+  const handleBlur = () => {
+    console.log('Hey! Eyes on me!');
+  };
+
+  return (
+    <button
+      onFocus={handleFocus} // Attach the focus event handler
+      onBlur={handleBlur}   // Attach the blur event handler
+    >
+      Eyes on me
+    </button>
+  );
+}
+
+export default EyesOnMe;
